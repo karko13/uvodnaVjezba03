0f2886233e0318d3295e480147005c66755ab39d
karko13 <145532713+karko13@users.noreply.github.com>
2023-09-20 09:31:54 +0200

    Add files via upload

---
+++ uvodnaVjezba03.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uvodnaVjezba03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            ajde:
+            Console.WriteLine(" molim vas upišite prvi broj:  ");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("molim vas upisite drugi broj: ");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("molim  vas upisite treci broj: ");
+            c = double.Parse(Console.ReadLine());
+            if(a <=0)
+            {
+                Console.WriteLine("stavili ste broj koji se ne moze koristiti ");
+                goto ajde;
+            }
+            if(b <=0)
+            {
+                Console.WriteLine("stavili ste broj koji se ne moze koristitit ");
+                goto ajde;
+            }
+            if(c <=0) 
+            {
+                Console.WriteLine("stavili ste broj koji se ne moze korititi ");
+                goto ajde;
+            }
+            if((c*c)==(a*a)+(b*b))
+            {
+                Console.WriteLine("trokut je pravokutan BRAVO!!!");
+            }
+            else
+            {
+                Console.WriteLine(" trokut nije pravokutan ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
